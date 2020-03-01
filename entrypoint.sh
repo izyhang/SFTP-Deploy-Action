d90d0854449f00046004cb0bdbaae179d897a44b
zyhang <izyhang@gmail.com>
2020-03-01 15:02:13 +0800

    modift inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,31 +3,45 @@
 #set -e at the top of your script will make the script exit with an error whenever an error occurs (and is not explicitly handled)
 set -eu
 
+echo 'deploy start'
+
+SFTP_USER_NAME=$1
+SFTP_SERVER=$2
+SFTP_PORT=$3
+SSH_PRIVATE_KEY=$4
+SFTP_SRC=$5
+SFTP_DEST_SRC=$6
+SFTP_ARGS=$7
+SSH_COMMAND=$8
 
 TEMP_SSH_PRIVATE_KEY_FILE='../private_key.pem'
 TEMP_SFTP_FILE='../sftp'
 
 # keep string format
-printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
+printf "%s" "$SSH_PRIVATE_KEY" >$TEMP_SSH_PRIVATE_KEY_FILE
 # avoid Permissions too open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'sftp start'
 
 # create a temporary file containing sftp commands
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
+printf "%s" "put -r $SFTP_SRC $SFTP_DEST_SRC" >$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b $TEMP_SFTP_FILE -P $SFTP_PORT $SFTP_ARGS -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $SFTP_USER_NAME@$SFTP_SERVER
 
-echo 'deploy success'
+echo 'sftp success'
 
-if [ -z "$8" ]
+echo 'checking ssh command'
+
+if [ -z "$SSH_COMMAND" ]
 then
     echo 'no ssh command'
 else
     echo 'ssh command start'
-    ssh -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 "cd $6;$8"
+    ssh -p $SFTP_PORT -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $SFTP_USER_NAME@$SFTP_SERVER "cd $SFTP_DEST_SRC;$SSH_COMMAND"
     echo 'ssh command success'
 fi
+
+echo 'deploy success'
 
 exit 0
